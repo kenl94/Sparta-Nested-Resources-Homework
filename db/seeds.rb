d0778f47bddebe32ce238edc64962972f99eb7dc
kenl94 <KLy@spartaglobal.com>
2018-06-23 22:04:30 +0100

    Added faker to generate the data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  Superhero.create(name: Faker::Superhero.name, age: Faker::Number.between(18, 40), state: Faker::Address.state)
+end
+10.times do
+  randomsuperhero = rand(1..Superhero.all.length)
+  Ability.create(power: Faker::Superhero.power, superhero_id: randomsuperhero)
+end
